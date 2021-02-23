263a358d437c5ee406a205443c3b23dd22d367d1
Kirill Gurlev <kirgurkir@mail.ru>
2021-02-23 13:57:26 +0300

    14_pushes_Exceptions

--- app/src/main/java/ru/netology/view/activity/AppActivity.kt
+++ app/src/main/java/ru/netology/view/activity/AppActivity.kt
@@ -1,6 +1,29 @@
 package ru.netology.view.activity
 
+import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import ru.netology.R
 
-class AppActivity : AppCompatActivity(R.layout.app_activity)
+class AppActivity : AppCompatActivity(R.layout.app_activity) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("NMEDIA PUSH TOKEN", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = token.toString()
+            Log.d("NMEDIA PUSH TOKEN", msg)
+        })
+    }
+}
